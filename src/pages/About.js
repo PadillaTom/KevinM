b74d91d69d43fdac03a22f3ec11d3ecf8e6f545c
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-02 10:30:52 +0200

    Mobile done

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,23 +5,25 @@ export default function About() {
   return (
     <section className='section about-section'>
       <div className='section-center'>
-        <h1 className='section-title'>
-          Qui sommes-nous <span>?</span>
-        </h1>
-        <p className='about-text'>
-          NBC est en entreprise spécialisée en évènement cocktails privée. Mené
-          par
-          <Link to='/Kevin' className='about-kevin'>
-            Kevin
+        <div className='about-section-container'>
+          <h1 className='section-title'>
+            Qui sommes-nous <span>?</span>
+          </h1>
+          <p className='about-text'>
+            NBC est en entreprise spécialisée en évènement cocktails privée.
+            Mené par
+            <Link to='/Kevin' className='about-kevin'>
+              Kevin
+            </Link>
+            , un bartender passionné et spécialisé, fort d’une belle et riche
+            expérience dans le métier, NBC est une équipe jeune, dynamique avec
+            de forte personnalités formé et spécialisé dans le milieu de la
+            mixologie et les évènements privé.
+          </p>
+          <Link to='/cocktails' className='btn btn-about'>
+            Cocktails
           </Link>
-          , un bartender passionné et spécialisé, fort d’une belle et riche
-          expérience dans le métier, NBC est une équipe jeune, dynamique avec de
-          forte personnalités formé et spécialisé dans le milieu de la mixologie
-          et les évènements privé.
-        </p>
-        <Link to='/cocktails' className='btn btn-about'>
-          Cocktails
-        </Link>
+        </div>
       </div>
     </section>
   );
